08be1daf96ee143c842c4f68c3941586042d7578
Axel Marcial <axelmarcial151003@gmail.com>
2023-11-17 13:32:33 +0100

    Revert "return to merge"

--- Assets/Scripts/InvadersManager.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class InvadersManager : MonoBehaviour
-{
-    public List<Mesh> invadersMeshes = new List<Mesh>();
-    public List<Material> invadersMaterials = new List<Material>();
-}
